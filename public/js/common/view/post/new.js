09a69806ad48b73c3784baa79658ccc64f9b0bec
ding.zhou <ts-ding.zhou@mail.rakuten.com>
2014-07-28 18:55:47 +0900

    development commit

---
+++ public/js/common/view/post/new.js
@@ -0,0 +1,136 @@
+define([
+    'text!common/template/post/new.html',
+], function(
+    template
+) {
+
+    return Backbone.Marionette.ItemView.extend({
+
+        // template
+        template: template,
+
+        // class name
+        className: 'widget-box widget-color-green',
+
+        // ui
+        ui: {
+            newPost: '.wysiwyg-editor',
+            btnPost: '.btn-post'
+        },
+
+        // events
+        events: {
+            'click .btn-post': 'onPost',
+            'keyup .wysiwyg-editor': 'enablePost'
+        },
+
+        // initializer
+        initialize: function() {
+
+            // // create posts collection
+            // this.collection = new PostsCollection(null, {document: selink.user});
+
+            // // create region manager (this composite view will have layout ability)
+            // this.rm = new Backbone.Marionette.RegionManager();
+
+            // // create regions
+            // this.regions = this.rm.addRegions({
+            //     groupListRegion: '#group-list',
+            // });
+
+            // // create group drop-down list view
+            // this.groupListView = new GroupListView();
+
+            // // listen to the group list view, for the selection of target group
+            // this.listenTo(this.groupListView, 'group-select', this.setGroup);
+            // this.listenTo(this.groupListView, 'group-clear', this.unsetGroup);
+        },
+
+        // After show
+        onShow: function() {
+
+            // display group drop-down list
+            // this.regions.groupListRegion.show(this.groupListView);
+        },
+
+        // on render
+        onRender: function() {
+
+            // initiate wysiwyg eidtor for post edit area
+            this.ui.newPost.ace_wysiwyg({
+                toolbar_place: function(toolbar) {
+                    return $(this).closest('.widget-box').find('.btn-toolbar').prepend(toolbar).children(0).addClass('inline');
+                }
+            }).prev().addClass('wysiwyg-style3');
+
+            // this.ui.newPost.niceScroll();
+        },
+
+        // before destroy
+        onBeforeDestroy: function() {
+            // destroy region manager
+            // this.rm.destroy();
+        },
+
+        // hold the selected group
+        setGroup: function(model) {
+            this.targetGroup = model;
+        },
+
+        // clear the target group
+        unsetGroup: function(event) {
+            this.targetGroup = null;
+        },
+
+        // change the status of post button
+        enablePost: function() {
+
+            // get user input
+            var input = this.ui.newPost.cleanHtml();
+
+            // if user input is not empty
+            if (input && !_.str.isBlank(input)) {
+                // enable the post button
+                this.ui.btnPost.removeClass('disabled');
+            } else {
+                // disable ths post button
+                this.ui.btnPost.addClass('disabled');
+            }
+        },
+
+        // new post
+        onPost: function() {
+
+            var self = this,
+                post = {
+                    content: this.ui.newPost.cleanHtml()
+                };
+
+            if (this.targetGroup)
+                post.group = this.targetGroup.id;
+
+            // create new post
+            this.collection.create(post, {
+                success: function(model, response, options) {
+                    // clear input area
+                    self.ui.newPost.html("");
+                    // disable post button (can't post empty)
+                    self.ui.btnPost.addClass('disabled');
+                },
+                error: function(model, xhr, options) {
+
+                    if (xhr.status === 413)
+                        // show error
+                        $.gritter.add({
+                            title: '投稿は失敗しました',
+                            text: 'ご投稿した内容のサイズは大きすぎたため、投稿は受入ませんでした。画像を含めて投稿する場合は、直接に画像を挿入せずに、画像リンクをご利用ください。',
+                            class_name: 'gritter-error',
+                            time: 12000,
+                        });
+                },
+                wait: true
+            });
+        }
+
+    });
+});
